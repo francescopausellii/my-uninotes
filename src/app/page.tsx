fc6f91a9e429bf1d259cbc6f35c31ee572f9dfc7
cecco25 <cecchinimichael.code@gmail.com>
2025-05-11 22:42:13 +0200

    fix(Unused error): added run build in precommit and fixed unused error in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 import LogoutButton from "@/components/logout-button";
 
 export default async function Home() {
   const supabase = await createClient();
   const { data, error } = await supabase.auth.getUser();
+
+  if (error || !data?.user) {
+    redirect("/login");
+  }
 
   return (
     <main className="p-4">
